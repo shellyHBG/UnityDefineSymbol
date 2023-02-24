c7f2bbd86c5a5a1df7e3216225052f8f9507be33
xunyihuang <k820720k.cute@gmail.com>
2023-02-24 22:43:12 +0800

    Setup build group in constructor.

--- TestDefineSymbol.cs
+++ TestDefineSymbol.cs
@@ -6,8 +6,7 @@ public class TestDefineSymbol
     [MenuItem("Unit/Builder/DefineSymbols/Add sy1")]
     public static void Add_sy1()
     {
-        BuildTargetGroup group = BuildPipeline.GetBuildTargetGroup(EditorUserBuildSettings.activeBuildTarget);
-        ISymbolProvider symbol = new UnitySymbolProvider(group);
+        ISymbolProvider symbol = new UnitySymbolProvider();
 
         DefineSymbolCollection collection = new DefineSymbolCollection(symbol);
         collection.Add("sy1");
@@ -16,8 +15,7 @@ public class TestDefineSymbol
     [MenuItem("Unit/Builder/DefineSymbols/Remove sy1")]
     public static void Remove_sy1()
     {
-        BuildTargetGroup group = BuildPipeline.GetBuildTargetGroup(EditorUserBuildSettings.activeBuildTarget);
-        ISymbolProvider symbol = new UnitySymbolProvider(group);
+        ISymbolProvider symbol = new UnitySymbolProvider();
 
         DefineSymbolCollection collection = new DefineSymbolCollection(symbol);
         collection.Remove("sy1");
